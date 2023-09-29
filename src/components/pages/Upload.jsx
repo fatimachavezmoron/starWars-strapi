8837a268bf12f754e7f90f6945a81fc9d8118084
Fatima Chavez <fatimachavezmoron@gmail.com>
2023-09-29 13:02:42 +0200

    user upload complete

--- src/components/pages/Upload.jsx
+++ src/components/pages/Upload.jsx
@@ -4,9 +4,9 @@ import { useState } from 'react';
 
 const Upload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-  // const [title, setTitle] = useState('');
-  // const [description, setDescription] = useState('');
-  // const [price, setPrice] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -21,23 +21,25 @@ const Upload = () => {
 
     axios.post("http://localhost:1337/api/upload", fileData)
       .then((response) => {
-        console.log(response)
         const fileId = response.data[0].id;
 
         // 2. Enviar los datos del formulario junto con el ID del archivo a Strapi
-        let formData = new FormData();
-        // formData.append("title", title);
-        // formData.append("description", description);
-        // formData.append("price", price);
-        formData.append("file", fileId); // Asegúrate de que el campo coincide con el que has definido en tu modelo
+        // let formData = new FormData();
+        const formData = { data: {} }
+        formData.data.title = title;
+        formData.data.description = description;
+        formData.data.price = price;
+        formData.data.image = fileId;
 
-        // axios.post("http://localhost:1337/api/products", formData)
-        //   .then((response) => {
-        //     console.log("Formulario enviado con éxito", response.data);
-        //   })
-        //   .catch((error) => {
-        //     console.error("Error al enviar el formulario", error);
-        //   });
+        console.log('formData', formData)
+        
+        axios.post("http://localhost:1337/api/products", formData)
+          .then((response) => {
+            console.log("Formulario enviado con éxito", response.data);
+          })
+          .catch((error) => {
+            console.error("Error al enviar el formulario", error);
+          });
       })
       .catch((error) => {
         console.error("Error al subir el archivo", error);
@@ -49,7 +51,7 @@ const Upload = () => {
       <div className='formCont'>
         <div className='inputCont'>
           <form onSubmit={handleFormSubmit}>
-            {/* <Input type="text" value={title} 
+            <Input type="text" value={title} 
             onChange={(e) => setTitle(e.target.value)} 
             placeholder="Title" />
             <br />
@@ -58,7 +60,7 @@ const Upload = () => {
             <br />
             <Input type="number" value={price} 
             onChange={(e) => setPrice(e.target.value)} placeholder="Price" />
-            <br /> */}
+            <br />
             <Input type="file" 
             onChange={handleFileChange} style={{ color: 'white', backgroundColor: 'gray' }} />
             <br />
